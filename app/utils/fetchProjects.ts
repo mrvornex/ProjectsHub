010e9ac6a5e4a4e331d1ee0c82f20cc378f7c920
mrvornex <bilalali.office.pk@gmail.com>
2026-06-04 10:34:32 +0500

    Updated

--- app/utils/fetchProjects.ts
+++ app/utils/fetchProjects.ts
@@ -19,7 +19,7 @@ export const fetchProjects = async (): Promise<Project[]> => {
 
   for (const category of categories) {
     try {
-      const res = await fetch(`https://api.github.com/repos/Bilal742/JavaScript-projects/contents/projects/${category}`);
+      const res = await fetch(`https://api.github.com/repos/mrvornex/JavaScript-projects/contents/projects/${category}`);
       const data = await res.json();
 
       for (const item of data) {
@@ -27,14 +27,14 @@ export const fetchProjects = async (): Promise<Project[]> => {
 
         let meta: any = {};
         try {
-          const metaRes = await fetch(`https://raw.githubusercontent.com/Bilal742/JavaScript-projects/main/projects/${category}/${item.name}/meta.json`);
+          const metaRes = await fetch(`https://raw.githubusercontent.com/mrvornex/JavaScript-projects/main/projects/${category}/${item.name}/meta.json`);
           if (metaRes.ok) meta = await metaRes.json();
-        } catch { }
+        } catch { }  
 
         const image = meta.image
           ? meta.image.startsWith("http")
             ? meta.image
-            : `https://raw.githubusercontent.com/Bilal742/JavaScript-projects/main/projects/${category}/${item.name}/${meta.image.replace("./", "")}`
+            : `https://raw.githubusercontent.com/mrvornex/JavaScript-projects/main/projects/${category}/${item.name}/${meta.image.replace("./", "")}`
           : `https://opengraph.githubassets.com/1/Bilal742/${item.name}`;
 
         allProjects.push({
@@ -44,7 +44,7 @@ export const fetchProjects = async (): Promise<Project[]> => {
           description: meta.description ?? "No description available",
           image,
           github: item.html_url,
-          live: `https://bilal742.github.io/JavaScript-projects/projects/${category}/${item.name}/index.html`,
+          live: `https://mrvornex.github.io/JavaScript-projects/projects/${category}/${item.name}/index.html`,
           techStack: meta.techStack ?? [],
           features: meta.features ?? [],
         });
